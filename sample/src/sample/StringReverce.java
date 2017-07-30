ca2ef6fd62ffe5780ec5eff3f0031eae9af2286c
mohan <mohansai9009@gmail.com>
2017-07-30 15:51:03 +0530

    add sample project

---
+++ sample/src/sample/StringReverce.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class StringReverce {
+	
+	public void add(int a,int b)
+	{
+	System.out.println(a+b +"it is sum of a and b");	
+	}
+	public void add(float a,float b)
+	{
+	System.out.println(a+b +"it is sum of a and b");	
+	}
+	
+	
+public static void main(String[] args) {
+	StringReverce s=new StringReverce();
+	s.add(1,6);
+}
+}
